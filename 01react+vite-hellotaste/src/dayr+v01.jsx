d84ae83cd739ba322dbabaed5c530708005ab531
nihalraza369 <nihalraza369@gmail.com>
2025-05-06 16:04:49 +0500

    DAY 01 REACT+VITE

---
+++ 01react+vite-hellotaste/src/dayr+v01.jsx
@@ -0,0 +1,10 @@
+ function Myfunction() {
+    return (
+        <div>
+            <h1>Day01 REACT+VITE</h1>
+            <p>WELCOME TO REACT+VITE</p>
+        </div>
+    );
+
+}
+export default Myfunction;
