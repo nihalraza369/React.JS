3e96378a44cd40826febd5b5c72f50a819b88392
nihalraza369 <nihalraza369@gmail.com>
2025-05-06 19:16:55 +0500

    main file

--- 01react+vite-hellotaste/src/main.jsx
+++ 01react+vite-hellotaste/src/main.jsx
@@ -1,9 +1,36 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+
+
+/*
+createRoot(document.getElementById('root')).render(
+    App()
+)
+*/
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+/*
+//=====================================================DAY02==========================================
+
+const another= React.createElement(
+  'a',
+  {
+    href: 'https://www.google.com',
+    target: '_blank',
+  },
+  'visit google'
+)
+  
+
+createRoot(document.getElementById('root')).
+render(
+  <StrictMode>
+    {another}
+  </StrictMode>
+)*/
