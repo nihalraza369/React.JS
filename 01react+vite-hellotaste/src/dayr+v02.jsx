941d633c8d21178f359f4878f4b41fcaf1604c28
nihalraza369 <nihalraza369@gmail.com>
2025-05-06 19:16:33 +0500

    dayr+v02.jsx

---
+++ 01react+vite-hellotaste/src/dayr+v02.jsx
@@ -0,0 +1,22 @@
+function MyFunction1() {
+    return (
+        <div>
+        <h1>day02 react+vite</h1>
+        <p>Welcome to the second day of learning React with Vite!</p>
+        </div>
+    );
+}
+
+export default MyFunction1;
+
+/*
+const Reactelement={
+    type:'a',
+    props: {
+        href:'https://www.google.com',
+        target:'_blank',  
+    },
+    Children:"visit google"
+  }
+    */
+
