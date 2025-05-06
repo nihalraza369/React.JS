90b7040ff8ceaf2c97f06c60e85e090035542496
nihalraza369 <nihalraza369@gmail.com>
2025-05-06 19:15:55 +0500

    App.jsx

--- 01react+vite-hellotaste/src/App.jsx
+++ 01react+vite-hellotaste/src/App.jsx
@@ -1,11 +1,21 @@
 import Myfunction from "./dayr+v01"
+import Reactelement from "./dayr+v02"
+import MyFunction1 from "./dayr+v02"
+
 
 
 function App() {
   return (
- <Myfunction />
+    <>
+      {Reactelement}
+      <Myfunction />
+      <MyFunction1 />
+      
+    </>
       
   )
 }
 
 export default App
+
+
