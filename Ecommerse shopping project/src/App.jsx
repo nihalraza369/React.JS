e0a7c7bdf34d01789765697246136ac7be724bf6
nihalraza369 <nihalraza369@gmail.com>
2025-05-08 15:25:10 +0500

    project

---
+++ Ecommerse shopping project/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      <div className="bg-gray-100 min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
