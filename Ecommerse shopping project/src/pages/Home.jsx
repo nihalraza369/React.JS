d703ef2d4e19ebd7cbfbc6a836a0aded003b7e0a
nihalraza369 <nihalraza369@gmail.com>
2025-05-08 15:27:05 +0500

    project

---
+++ Ecommerse shopping project/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeroSlider from '../components/HeroSlider';
+import SearchBar from '../components/SearchBar';
+
+const Home = () => {
+  return (
+    <div>
+      <HeroSlider />
+      <SearchBar />
+    </div>
+  );
+};
+
+export default Home;
